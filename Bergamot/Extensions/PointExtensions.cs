0dc5f341bdb8a2718595d7642b8f3579f195821e
Dmitry Terekhov <keltarhelviett@gmail.com>
2019-03-01 11:12:39 +1000

    Add image point-cloud boundaries calculation

---
+++ Bergamot/Extensions/PointExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace Bergamot.Extensions
+{
+    public static class PointExtensions
+    {
+        public static Point Add(this Point self, Point other)
+        {
+            return new Point(self.X + other.X, self.Y + other.Y);
+        }
+    }
+}
---
+++ Bergamot/NonConvexHullGeneration/NonConvexHull.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using Bergamot.Extensions;
+
+namespace Bergamot.NonConvexHullGeneration
+{
+    public static class NonConvexHull
+    {
+        public static List<Point> GetBoundaries(Bitmap image)
+        {
+            return SquareTrace(image);
+        }
+
+        public static List<Point> SquareTrace(Bitmap image)
+        {
+            var start = GetStartPoint(image);
+            var boundaryPoints = new List<Point> { start };
+            Point nextStep = GoLeft(new Point(1, 0));
+            Point next = start.Add(nextStep);
+            while (next != start) {
+                if (
+                    (next.X < 0 || next.Y < 0 || next.X >= image.Width || next.Y >= image.Height) ||
+                    image.GetPixel(next.X, next.Y).A == 0
+                ) {
+                    nextStep = GoRight(nextStep);
+                    next = next.Add(nextStep);
+                } else {
+                    boundaryPoints.Add(next);
+                    nextStep = GoLeft(nextStep);
+                    next = next.Add(nextStep);
+                }
+            }
+            return boundaryPoints;
+        }
+
+        private static Point GoLeft(Point p) => new Point(p.Y, -p.X);
+        private static Point GoRight(Point p) => new Point(-p.Y, p.X);
+
+        public static Point GetStartPoint(Bitmap bitmap)
+        {
+            for (int y = 0; y < bitmap.Height; y++) {
+                for (int x = 0; x < bitmap.Width; x++) {
+                    if (bitmap.GetPixel(x, y).A != 0) {
+                        return new Point(x, y);
+                    }
+                }
+            }
+            return new Point(-1, -1);
+        }
+    }
+}
